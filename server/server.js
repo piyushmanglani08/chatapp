1b94fe23bf00b335442467958b05214cd711fd93
Piyush Manglani <piyushmanglani@Piyushs-MacBook-Pro.local>
2022-04-13 11:13:02 -0400

    error fix

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const path = require('path');
 const express = require('express');
  
 const publicPath = path.join(__dirname, "/../public");
-const port = process.env.PORT || 3000;
-const host = '0.0.0.0';
 
 var app = express();
-
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log(`${port} is running.`);
-})
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number, ()=>{
+    console.log("working now");
+});
