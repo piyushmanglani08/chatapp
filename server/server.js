599f119e6291286d9b6d73de1a201f6a3d4ec43f
Piyush Manglani <piyushmanglani@Piyushs-MacBook-Pro.local>
2022-04-13 11:09:15 -0400

    error fix

--- server/server.js
+++ server/server.js
@@ -3,12 +3,8 @@ const express = require('express');
  
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000;
-
-const host = 'localhost';
-const port = 3000;
-
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(express.static(publicPath));
