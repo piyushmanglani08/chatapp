1c457bdf96303efa3a0ea56fd685dfd471cf335e
Piyush Manglani <piyushmanglani@Piyushs-MacBook-Pro.local>
2022-04-13 10:57:22 -0400

    changes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const path = require('path');
 const express = require('express');
  
 const publicPath = path.join(__dirname, "/../public");
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
+const host = 'localhost';
+const port = 3000;
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(3000, ()=>{
-    console.log("server is up at ${port}");
+    console.log(`${port} is running.`);
 })
