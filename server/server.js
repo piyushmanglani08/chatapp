702196ec792b1561a533e3e3c1255251173314c6
Piyush Manglani <piyushmanglani@Piyushs-MacBook-Pro.local>
2022-04-13 11:15:01 -0400

    blah

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const path = require('path');
 const express = require('express');
  
 const publicPath = path.join(__dirname, "/../public");
+const port = process.env.PORT || 3000;
 
 var app = express();
+
 app.use(express.static(publicPath));
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number, ()=>{
-    console.log("working now");
-});
+app.listen(port, ()=>{
+    console.log(`${port} is running.`);
+})
+
